f7f3c6b63bf9a2f11e1812c99e34df94226bc4db
the-moezzat <dev.moezzat@gmail.com>
2023-09-26 17:00:33 +0300

    feat: change the layout of current section

--- src/features/current/mainCurrent.tsx
+++ src/features/current/mainCurrent.tsx
@@ -9,8 +9,8 @@ function MainCurrent() {
   });
 
   return (
-    <div className="my-8">
-      <div className="flex flex-col gap-4">
+    <div className="my-8 max-w-3xl mx-auto px-4">
+      <div className="flex flex-row flex-wrap gap-x-20   gap-y-10 min-w-min mx-auto ">
         {sectionsLoading && <Loading size="medium" type="full" />}
         {data &&
           data.map((section) => <Section key={section.id} section={section} />)}
